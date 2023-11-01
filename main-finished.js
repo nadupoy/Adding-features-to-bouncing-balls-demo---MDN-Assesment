29aa560764204cb693bac0240e377a0beb77eb36
Grace <gnsampao@gmail.com>
2023-11-01 12:05:21 +0300

    Updated the collisionDetect() method to include the 'exists' property.

--- main-finished.js
+++ main-finished.js
@@ -27,11 +27,13 @@ class Shape {
 }
 
 class Ball extends Shape {
+    exists = true;
+
     constructor(x, y, velX, velY, color, size, exists) {
         super(x, y, velX, velY);
         this.color = color;
         this.size = size;
-        this.exists = true;
+        this.exists = exists;
     }
 
     draw() {
@@ -64,7 +66,7 @@ class Ball extends Shape {
 
     collisionDetect() {
         for (const ball of balls) {
-            if (!(this === ball)) {
+            if (!(this === ball) && ball.exists) {
                 const dx = this.x - ball.x;
                 const dy = this.y - ball.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
